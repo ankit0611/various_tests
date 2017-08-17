39dc60b3cb51b0fef257cf402aa28b44a2853ec2
srivastava <srivastava@gipmbh.de>
2017-08-17 15:30:22 +0200

    Initial Commit.

---
+++ src/main/java/Test_Pentaho_Dialog_Class.java
@@ -0,0 +1,16 @@
+
+public class Test_Pentaho_Dialog_Class {
+	// Eclipse sdk needs to be installed.
+	// http://archive.eclipse.org/eclipse/downloads/drops4/R-4.2.1-201209141800/index.php#EclipseSDK
+
+	public static void main(String[] args) /*throws KettleException*/ {
+		/*KettleEnvironment.init();
+		Display display = new Display();
+		PropsUI.init(display, "C:/Users/Srivastava" + "/.kettle/.spoonrc");
+		Shell shell = new Shell(display);
+		ICQueryMeta meta = new ICQueryMeta();
+		TransMeta tm = new TransMeta("C:/GIP/LocalTransformations/PluginTest.ktr");
+		ICQueryDialog ld = new ICQueryDialog(shell, meta, tm, "steptest");
+		ld.open();*/
+	}
+}
---
+++ src/main/java/Validate_URL_Regular_Expression.java
@@ -0,0 +1,23 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Validate_URL_Regular_Expression {
+
+	public static void main(String[] args) {
+		String regex = "^(https?|ftp|file)://[-a-zA-Z0-9+&@#/%?=~_|!:,.;]*[-a-zA-Z0-9+&@#/%=~_|]";
+		regex = "\\b(https?|ftp|file)://[-a-zA-Z0-9+&@#/%?=~_|!:,.;]*[-a-zA-Z0-9+&@#/%=~_|]";
+		
+		System.out.println(IsMatch("http://sro-misc1-gb:52180",regex));
+	}
+
+	private static boolean IsMatch(String s, String pattern) {
+		try {
+			Pattern patt = Pattern.compile(pattern);
+			Matcher matcher = patt.matcher(s);
+			return matcher.matches();
+		} catch (RuntimeException e) {
+			return false;
+		}
+
+	}
+}
