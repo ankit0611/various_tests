8f5e5044c43942b529f91318f41f442255c8c60a
srivastava <srivastava@gipmbh.de>
2017-08-30 17:33:24 +0200

    split a string at firt occurance of a character

---
+++ src/main/java/Split_String_At_First_Occurance.java
@@ -0,0 +1,13 @@
+
+public class Split_String_At_First_Occurance {
+
+	public static void main(String[] args) {
+
+		String test_object = "jdbc:host:8080";
+		String[] output_objects = test_object.split(":", 2);
+
+		for (String output_object : output_objects)
+			System.out.println(output_object);
+	}
+
+}
