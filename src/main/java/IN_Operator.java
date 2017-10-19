a715abe88e441d641bd7ad212a8ef9ff9993bfd2
srivastava <srivastava@gipmbh.de>
2017-10-19 14:47:01 +0200

    In operator

---
+++ src/main/java/IN_Operator.java
@@ -0,0 +1,14 @@
+/*
+ * IN_Operator.java
+ *
+ * Erstellung     : 19.10.2017 / Srivastava
+ *
+ * Copyright © by GiP mbH
+ */
+
+public class IN_Operator {
+    public static void main(String[] args) {
+        System.out.println("");
+    }
+
+}
