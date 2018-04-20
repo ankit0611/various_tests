02d2e2b1d422b00efac0fe633e4a19a93d240cde
srivastava <srivastava@gipmbh.de>
2018-04-20 14:31:44 +0200

    hindi exceptions

---
+++ src/main/java/HindiExceptions.java
@@ -0,0 +1,14 @@
+/*
+ * HindiExceptions.java
+ *
+ * Erstellung     : 16.03.2018 / Srivastava
+ *
+ * Copyright © by GiP mbH
+ */
+
+public class HindiExceptions {
+    public static void main(String[] args) throws Exception {
+        throw new Exception("कुछ गड़बड़ है");
+    }
+
+}
