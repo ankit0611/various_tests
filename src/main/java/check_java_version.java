22abcbe032bac88535720a5fba00966636b60b51
srivastava <srivastava@gipmbh.de>
2018-04-30 16:00:47 +0200

    check java version

---
+++ src/main/java/check_java_version.java
@@ -0,0 +1,16 @@
+/*
+ * check_java_version.java
+ *
+ * Erstellung     : 30.04.2018 / Srivastava
+ *
+ * Copyright © by GiP mbH
+ */
+
+public class check_java_version {
+
+    public static void main(String[] args) {
+        String version = System.getProperty("java.version");
+        System.out.println(version);
+    }
+
+}
