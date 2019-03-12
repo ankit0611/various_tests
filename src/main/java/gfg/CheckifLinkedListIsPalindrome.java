807cd268c23d25a1aa7740d6c622ffde66291563
srivastava <srivastava@gipmbh.de>
2019-03-12 12:05:21 +0100

    Palindrome Linked Lists

---
+++ src/main/java/gfg/CheckifLinkedListIsPalindrome.java
@@ -0,0 +1,95 @@
+package gfg;
+
+import java.util.Scanner;
+
+class CheckifLinkedListIsPalindrome {
+  Node head;
+
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int t = sc.nextInt();
+
+    while (t > 0) {
+      int n = sc.nextInt();
+      // int k = sc.nextInt();
+      CheckifLinkedListIsPalindrome llist = new CheckifLinkedListIsPalindrome();
+      // int n=Integer.parseInt(br.readLine());
+      int a1 = sc.nextInt();
+      Node head = new Node(a1);
+      llist.addToTheLast(head);
+      for (int i = 1; i < n; i++) {
+        int a = sc.nextInt();
+        llist.addToTheLast(new Node(a));
+      }
+
+      GfG g = new GfG();
+      if (GfG.isPalindrome(llist.head) == true) {
+        System.out.println(1);
+      } else {
+        System.out.println(0);
+      }
+      t--;
+    }
+  }
+
+  /* Function to print linked list */
+  static void printList(Node head) {
+    Node temp = head;
+    while (temp != null) {
+      System.out.print(temp.data + " ");
+      temp = temp.next;
+    }
+    System.out.println();
+  }
+
+  /* Inserts a new Node at front of the list. */
+  public void addToTheLast(Node node) {
+    if (head == null) {
+      head = node;
+    } else {
+      Node temp = head;
+      while (temp.next != null) {
+        temp = temp.next;
+      }
+      temp.next = node;
+    }
+  }
+}
+
+/*Please note that it's Function problem i.e.
+you need to write your solution in the form of Function(s) only.
+Driver Code to call/invoke your function is mentioned above.*/
+
+/* Structure of class Node is
+class Node
+{
+	int data;
+	Node next;
+
+	Node(int d)
+	{
+		data = d;
+		next = null;
+	}
+}*/
+class GfG {
+  static boolean isPalindrome(Node head) {
+    StringBuilder sb = new StringBuilder();
+    Node node = head;
+    while (node.next != null) {
+      sb.append(node.data);
+      node = node.next;
+    }
+    sb.append(node.data);
+
+    char[] chars = sb.toString().toCharArray();
+    int k;
+    for (int i = chars.length - 1; i > 0; i--) {
+      k = chars.length - (i + 1);
+      if (chars[i] != chars[k]) {
+        return false;
+      }
+    }
+    return true;
+  }
+}
